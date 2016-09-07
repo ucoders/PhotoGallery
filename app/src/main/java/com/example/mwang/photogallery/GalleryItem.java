62af97429d6ca2f300aa6ce0b12814aae189a055
Mingzhong Wang <mwang@usc.edu.au>
2016-09-08 08:59:15 +1000

    Chapter 23 completed

---
+++ app/src/main/java/com/example/mwang/photogallery/GalleryItem.java
@@ -0,0 +1,40 @@
+package com.example.mwang.photogallery;
+
+/**
+ * Created by mwang on 7/09/2016.
+ */
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+
+    @Override
+
+    public String toString() {
+        return mCaption;
+    }
+}
---
+++ app/src/main/java/com/example/mwang/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,11 @@
+package com.example.mwang.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
